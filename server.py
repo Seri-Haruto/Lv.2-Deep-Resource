ea4988a1a302ef661ae25a58a5de84df81a23c04
Seri-Haruto <seriharu41036@icloud.com>
2025-08-24 04:23:29 +0900

    0824-2

--- server.py
+++ server.py
@@ -133,6 +133,7 @@ def export_csv():
 
 if __name__ == '__main__':
     init_db()
-    port = int(os.environ.get("PORT", 18000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 10000)) 
+    app.run(host='0.0.0.0', port=port, debug=False)
+
 
